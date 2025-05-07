af6b7d66e12bb371a890634882d4686258f943f5
Sprenger07 <leo.ferretti@epita.fr>
2025-05-07 18:15:54 +0200

    add : encrypt method in API

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from typing import Union
+from typing import Dict
 
-from fastapi import FastAPI
+import base64
+import json
+
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -8,3 +11,26 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/encrypt")
+def post_encrypt(payload: Dict | None = None) -> Dict:
+    if payload is None:
+        raise HTTPException(status_code=400, detail="Invalid payload")
+
+    encrypted_data = {}
+    for key, value in payload.items():
+        if type(value) is dict:
+            encoded_value = base64.b64encode(
+                bytes(
+                    "{" + ", ".join(f'"{k}":"{v}"' for k, v in value.items()) + "}",
+                    "utf-8",
+                )
+            )
+        else:
+            encoded_value = base64.b64encode(bytes(str(value), "utf-8"))
+        encrypted_data.update({key: encoded_value})
+
+    return encrypted_data
+
+
