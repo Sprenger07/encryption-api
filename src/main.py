4622f1a1ef47d65d6783732b77ea90b0f0003860
Sprenger07 <leo.ferretti@epita.fr>
2025-05-07 22:46:52 +0200

    add : decrypt method in API

--- src/main.py
+++ src/main.py
@@ -29,3 +29,23 @@ def post_encrypt(payload: Dict | None = None) -> Dict:
         encrypted_data.update({key: encoded_value})
 
     return encrypted_data
+
+
+@app.post("/decrypt")
+def post_decrypt(payload: Dict | None = None) -> Dict:
+    if payload is None:
+        raise HTTPException(status_code=400, detail="Invalid payload")
+
+    decrypted_data = {}
+
+    for key, value in payload.items():
+        try:
+            decoded_value = base64.b64decode(value, validate=True)
+            try:
+                decrypted_data.update({key: json.loads(decoded_value)})
+                continue
+            except Exception:
+                decrypted_data.update({key: decoded_value})
+        except Exception:
+            decrypted_data.update({key: value})
+    return decrypted_data
