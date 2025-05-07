79ffb41459e602e455c10a298778fe75c7d4135e
Sprenger07 <leo.ferretti@epita.fr>
2025-05-08 00:12:56 +0200

    add : verify endpoint in API

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import json
 import hmac
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
@@ -68,3 +68,28 @@ def post_sign(payload: Dict | None = None) -> Dict:
     ).hexdigest()
 
     return {"signature": signature}
+
+
+@app.post("/verify", status_code=status.HTTP_204_NO_CONTENT)
+def verify_sign(payload: Dict | None = None) -> None:
+    if payload is None:
+        raise HTTPException(status_code=400, detail="Missing payload")
+
+    if payload.get("signature") is None:
+        raise HTTPException(status_code=400, detail="Missing signature")
+
+    if payload.get("data") is None:
+        raise HTTPException(status_code=400, detail="Missing data")
+
+    payload = dict(sorted(payload.items()))
+
+    secret_key = os.environ.get("SECRET_KEY")
+
+    signature = hmac.new(
+        bytes(secret_key, "utf-8"), bytes(json.dumps(payload), "utf-8"), hashlib.sha256
+    ).hexdigest()
+
+    if signature == payload.get("signature"):
+        return
+    else:
+        HTTPException(status_code=400, detail="Invalid signature")
